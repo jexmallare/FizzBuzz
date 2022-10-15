5602bbb4cbe56ae912e5901782f1603112f4c5c9
jexmallare <jexmallare@gmail.com>
2022-10-14 22:37:45 -0500

    Added comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,18 @@
+#Loop up to a hundred
 for i in range(1,101):
+#if divisible by 3 and 5, return FizzBuzz
     if i%15 == 0:
         print("FizzBuzz")
+
+#if divisible by 3, return Fizz
     elif i%3 == 0:
         print("Fizz")
+
+#if divisible by 5, return Buzz
     elif i%5 == 0:
         print("Buzz")
+
+#else, print only the number
     else:
         print(i)
+
